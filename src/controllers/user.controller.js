7e90ef4fdc07f0d33ef588f4720603c6978877ec
buddhathapa12 <buddhat645@gmail.com>
2021-12-09 23:20:02 +0545

    Added login functionality to the system

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,6 +24,23 @@ export async function createUser(req, res, next) {
       newUser,
     });
   } catch (err) {
-    console.log("Hello", err);
+    res.status(500).json(err);
+  }
+}
+
+export async function login(req, res, next) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("User not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("Wrong Password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
   }
 }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,22 +1,10 @@
 import User from "../models/user.model.js";
 import express from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { createUser, login } from "../controllers/user.controller.js";
 const router = express.Router();
 
-router.post(
-  "/register",
-  createUser
-  // async (req, res) => {
-  //   const user = await new User({
-  //     username: "Buddha",
-  //     email: "buddha@gmail.com",
-  //     password: "12345",
-  //   });
-  //   await user.save();
-  //   res.send("ok");
-  // }
-);
-
+router.post("/register", createUser);
+router.post("/login", login);
 router.get("/", (req, res) => {
   res.send("Hello world I am coming fro auth's route");
 });
